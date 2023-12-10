46c8fee596305227e50e4770d379cd9afff3566f
adexcel171 <godwillokenyi@gmail.com>
2023-12-10 09:33:43 +0100

    added how to send data to a domi api

--- consumeData.js
+++ consumeData.js
@@ -49,3 +49,77 @@ map: Extracts the titles from each item in the data.
 filter: Filters the data to include only posts with userId equal to 1.
 reduce: Calculates the total number of characters in all titles.
 Remember to replace the apiUrl with the actual API endpoint you want to fetch data from. Additionally, consider error handling for robustness in a real-world application.
+
+// for sending data to a domi api
+
+To send form values to a DOMI (Document Object Model Interface) API using HTTP methods like POST, PUT, and DELETE, you will typically use a combination of HTML, JavaScript, and possibly a server-side language if you're working in a web application context. Here's a basic example using JavaScript and the Fetch API:
+
+Assuming you have an HTML form like this:
+
+html
+Copy code
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Submission</title>
+</head>
+<body>
+  <form id="myForm">
+    <!-- Your form fields go here -->
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+    <br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    <br>
+    <!-- Add other form fields as needed -->
+    
+    <!-- Buttons to trigger actions -->
+    <button type="button" onclick="postData()">Submit (POST)</button>
+    <button type="button" onclick="putData()">Update (PUT)</button>
+    <button type="button" onclick="deleteData()">Delete (DELETE)</button>
+  </form>
+
+  <script>
+    async function postData() {
+      const formData = new FormData(document.getElementById('myForm'));
+      const response = await fetch('https://your-api-endpoint', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+
+    async function putData() {
+      const formData = new FormData(document.getElementById('myForm'));
+      const response = await fetch('https://your-api-endpoint', {
+        method: 'PUT',
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+
+    async function deleteData() {
+      const formData = new FormData(document.getElementById('myForm'));
+      const response = await fetch('https://your-api-endpoint', {
+        method: 'DELETE',
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+  </script>
+</body>
+</html>
+In this example:
+
+The FormData object is used to collect the form data.
+The fetch function is used to make asynchronous requests to the specified API endpoint.
+The method property of the fetch options is set to the desired HTTP method (POST, PUT, DELETE).
+The form data is passed as the body of the request.
+The response is handled, and you can perform further actions based on the API's response.
+Replace 'https://your-api-endpoint' with the actual endpoint of your DOMI API. Additionally, ensure that your API supports the specified HTTP methods and handles the incoming data accordingly. The server-side code on the API should be able to parse and handle form data for each method.
